70d6e2569e8798dfb657516d602de1c45838ff7d
Ben Kirschniak <ben@kirschniak.de>
2026-01-22 16:18:34 +0100

    support for multiple assembly files

--- build.rs
+++ build.rs
@@ -1,7 +1,26 @@
 use cc::Build;
 use std::env;
+use std::fs;
+use std::path::Path;
 
 fn main() {
     unsafe { env::set_var("CC", "/opt/homebrew/opt/llvm/bin/clang") };
-    Build::new().file("asm/boot.s").compile("asm");
+
+    let mut asm_files = Vec::<String>::new();
+    let asm_dir = fs::read_dir("asm").unwrap();
+    for entry in asm_dir {
+        let entry = entry.unwrap();
+        let file_name = entry.file_name();
+        let file_path = Path::new(&file_name);
+        let Some(extension) = file_path.extension() else {
+            continue;
+        };
+        if extension == "s" {
+            let mut file = "asm/".to_string();
+            file.push_str(file_path.to_str().unwrap());
+            asm_files.push(file);
+        }
+    }
+
+    Build::new().files(asm_files).compile("asm");
 }
